93114fce1cf9f3e5c98e95ec488169b0900537ef
ArtemK2008 <kalachev.artemm@gmail.com>
2022-12-21 13:06:16 +0300

    Initial version

---
+++ src/main/java/green_project/interview_helper/InterviewHelperApplication.java
@@ -0,0 +1,13 @@
+package green_project.interview_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewHelperApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(InterviewHelperApplication.class, args);
+  }
+
+}
