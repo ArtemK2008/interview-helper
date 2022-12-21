004341b856e32b252acdf07fb73371947dc9cf64
ArtemK2008 <kalachev.artemm@gmail.com>
2022-12-21 16:30:52 +0300

    All Entities created(might have some problems)

---
+++ src/main/java/com/project/green/entities/Question.java
@@ -0,0 +1,94 @@
+package com.project.green.entities;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+
+@Entity
+public class Question {
+
+  @Id
+  @Column(name = "question_id")
+  @SequenceGenerator(name = "question_seq", sequenceName = "question_sequence", initialValue = 1, allocationSize = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "question_seq")
+  private int id;
+  @Column(name = "question")
+  private String questionText;
+  @Column(name = "answer")
+  private String bestAnswer;
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "topic_id")
+  private Topic topic;
+  @OneToMany(cascade = CascadeType.MERGE, mappedBy = "question")
+  private Set<Answer> answers;
+  @ManyToMany
+  @JoinTable(name = "Save_question_to_person", joinColumns = {
+      @JoinColumn(name = "question_id") }, inverseJoinColumns = {
+          @JoinColumn(name = "person_id") })
+  private List<Person> personsWhoSavedThis;
+
+  public Question() {
+    super();
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getQuestionText() {
+    return questionText;
+  }
+
+  public void setQuestionText(String questionText) {
+    this.questionText = questionText;
+  }
+
+  public String getBestAnswer() {
+    return bestAnswer;
+  }
+
+  public void setBestAnswer(String bestAnswer) {
+    this.bestAnswer = bestAnswer;
+  }
+
+  public Topic getTopic() {
+    return topic;
+  }
+
+  public void setTopic(Topic topic) {
+    this.topic = topic;
+  }
+
+  public Set<Answer> getAnswers() {
+    return answers;
+  }
+
+  public void setAnswers(Set<Answer> answers) {
+    this.answers = answers;
+  }
+
+  public List<Person> getPersonsWhoSavedThis() {
+    return personsWhoSavedThis;
+  }
+
+  public void setPersonsWhoSavedThis(List<Person> personsWhoSavedThis) {
+    this.personsWhoSavedThis = personsWhoSavedThis;
+  }
+
+}
