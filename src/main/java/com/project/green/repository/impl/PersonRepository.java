88fd4c4b0146cb0fc9f6bf9efb15ef1cf478ba11
Пользователь <vladislav>
2022-12-28 18:01:22 +0300

    create crud person and base repository and controller advice

---
+++ src/main/java/com/project/green/repository/impl/PersonRepository.java
@@ -0,0 +1,9 @@
+package com.project.green.repository.impl;
+
+import com.project.green.entities.Person;
+import com.project.green.repository.BaseRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends BaseRepository<Person> {
+}
