bcc8e2f1affc0c29158d5f0ca993e4ce0bfc8177
ezzov <ezzov@yandex.ru>
2023-01-01 20:50:16 +0300

    Add hashCode, equals, toString to Entities

--- src/main/java/com/project/green/entities/Answer.java
+++ src/main/java/com/project/green/entities/Answer.java
@@ -1,7 +1,5 @@
 package com.project.green.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,78 +9,102 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
+import java.util.Objects;
 
 @Entity
 public class Answer {
 
-  @Id
-  @Column(name = "id")
-  @SequenceGenerator(name = "answer_seq", sequenceName = "answer_id_seq", initialValue = 1, allocationSize = 1)
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "answer_seq")
-  private int id;
+    @Id
+    @Column(name = "id")
+    @SequenceGenerator(name = "answer_seq", sequenceName = "answer_id_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "answer_seq")
+    private int id;
 
-  @Column(name = "voice_count")
-  private int voiceCount;
+    @Column(name = "voice_count")
+    private int voiceCount;
 
-  @Column(name = "answer")
-  private String answerText;
+    @Column(name = "answer")
+    private String answerText;
 
-  @Column(name = "isdefault")
-  private boolean isDefault;
+    @Column(name = "isdefault")
+    private boolean isDefault;
 
-  @JsonIgnore
-  @ManyToOne(cascade = CascadeType.ALL)
-  @JoinColumn(name = "question_id")
-  private Question question;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "question_id")
+    private Question question;
 
-  public Answer() {
-  }
+    public Answer() {
+    }
 
-  public Answer(int id, int voiceCount, String answerText, boolean isDefault, Question question) {
-    this.id = id;
-    this.voiceCount = voiceCount;
-    this.answerText = answerText;
-    this.isDefault = isDefault;
-    this.question = question;
-  }
+    public Answer(int id, int voiceCount, String answerText, boolean isDefault, Question question) {
+        this.id = id;
+        this.voiceCount = voiceCount;
+        this.answerText = answerText;
+        this.isDefault = isDefault;
+        this.question = question;
+    }
 
-  public int getId() {
-    return id;
-  }
+    public int getId() {
+        return id;
+    }
 
-  public void setId(int id) {
-    this.id = id;
-  }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public int getVoiceCount() {
-    return voiceCount;
-  }
+    public int getVoiceCount() {
+        return voiceCount;
+    }
 
-  public void setVoiceCount(int voiceCount) {
-    this.voiceCount = voiceCount;
-  }
+    public void setVoiceCount(int voiceCount) {
+        this.voiceCount = voiceCount;
+    }
 
-  public String getAnswerText() {
-    return answerText;
-  }
+    public String getAnswerText() {
+        return answerText;
+    }
 
-  public void setAnswerText(String answerText) {
-    this.answerText = answerText;
-  }
+    public void setAnswerText(String answerText) {
+        this.answerText = answerText;
+    }
 
-  public Question getQuestion() {
-    return question;
-  }
+    public Question getQuestion() {
+        return question;
+    }
 
-  public void setQuestion(Question question) {
-    this.question = question;
-  }
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
 
-  public boolean isDefault() {
-    return isDefault;
-  }
+    public boolean isDefault() {
+        return isDefault;
+    }
 
-  public void setDefault(boolean aDefault) {
-    isDefault = aDefault;
-  }
+    public void setDefault(boolean aDefault) {
+        isDefault = aDefault;
+    }
+
+    @Override
+    public String toString() {
+        return "Answer{" +
+                "id=" + id +
+                ", voiceCount=" + voiceCount +
+                ", answerText='" + answerText + '\'' +
+                ", isDefault=" + isDefault +
+                ", question=" + question +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Answer answer = (Answer) o;
+        return id == answer.id && voiceCount == answer.voiceCount && isDefault == answer.isDefault && answerText.equals(answer.answerText) && question.equals(answer.question);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, voiceCount, answerText, isDefault, question);
+    }
 }
--- src/main/java/com/project/green/entities/Person.java
+++ src/main/java/com/project/green/entities/Person.java
@@ -1,113 +1,149 @@
 package com.project.green.entities;
 
-import java.util.Set;
-
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.SequenceGenerator;
+import java.util.Objects;
+import java.util.Set;
 
 @Entity
 public class Person {
 
-  @Id
-  @Column(name = "id")
-  @SequenceGenerator(name = "person_seq", sequenceName = "person_id_seq", initialValue = 1, allocationSize = 1)
-  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "person_seq")
-  private int id;
+    @Id
+    @Column(name = "id")
+    @SequenceGenerator(name = "person_seq", sequenceName = "person_id_seq", initialValue = 1, allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "person_seq")
+    private int id;
 
-  @Column(name = "name")
-  private String fullName;
+    @Column(name = "name")
+    private String fullName;
 
-  @Column(name = "email")
-  private String email;
+    @Column(name = "email")
+    private String email;
 
-  @Column(name = "password")
-  private String password;
+    @Column(name = "password")
+    private String password;
 
-  @OneToOne(cascade = CascadeType.ALL, mappedBy="person")
-  private Statistics statistics;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "person")
+    private Statistics statistics;
 
-  @ManyToMany(cascade = CascadeType.MERGE, mappedBy = "personsWhoSavedThis")
-  private Set<Question> savedQuestions;
+    @ManyToMany(cascade = CascadeType.MERGE, mappedBy = "personsWhoSavedThis")
+    private Set<Question> savedQuestions;
 
-//  @ManyToMany(cascade = CascadeType.MERGE, mappedBy = "people")
-//  private Set<Role> roles;
+    @ManyToMany(cascade = CascadeType.MERGE, mappedBy = "people")
+    private Set<Role> roles;
 
-  @ManyToMany(cascade = CascadeType.MERGE, mappedBy = "people")
-  private Set<Topic> topics;
+    @ManyToMany(cascade = CascadeType.MERGE, mappedBy = "people")
+    private Set<Topic> topics;
 
-  public Person() {
-    super();
-  }
+    public Person() {
+    }
 
-  public int getId() {
-    return id;
-  }
+    public Person(int id, String fullName, String email, String password, Statistics statistics,
+                  Set<Question> savedQuestions, Set<Role> roles, Set<Topic> topics) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.statistics = statistics;
+        this.savedQuestions = savedQuestions;
+        this.roles = roles;
+        this.topics = topics;
+    }
 
-  public void setId(int id) {
-    this.id = id;
-  }
+    public int getId() {
+        return id;
+    }
 
-  public String getFullName() {
-    return fullName;
-  }
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public void setFullName(String fullName) {
-    this.fullName = fullName;
-  }
+    public String getFullName() {
+        return fullName;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public Statistics getStatistics() {
-    return statistics;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-  public void setStatistics(Statistics statistics) {
-    this.statistics = statistics;
-  }
+    public Statistics getStatistics() {
+        return statistics;
+    }
 
-  public Set<Question> getSavedQuestions() {
-    return savedQuestions;
-  }
+    public void setStatistics(Statistics statistics) {
+        this.statistics = statistics;
+    }
 
-  public void setSavedQuestions(Set<Question> savedQuestions) {
-    this.savedQuestions = savedQuestions;
-  }
+    public Set<Question> getSavedQuestions() {
+        return savedQuestions;
+    }
 
-//  public Set<Role> getRoles() {
-//    return roles;
-//  }
-//
-//  public void setRoles(Set<Role> roles) {
-//    this.roles = roles;
-//  }
+    public void setSavedQuestions(Set<Question> savedQuestions) {
+        this.savedQuestions = savedQuestions;
+    }
 
-  public Set<Topic> getTopics() {
-    return topics;
-  }
+    public Set<Role> getRoles() {
+        return roles;
+    }
 
-  public void setTopics(Set<Topic> topics) {
-    this.topics = topics;
-  }
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+    public Set<Topic> getTopics() {
+        return topics;
+    }
+
+    public void setTopics(Set<Topic> topics) {
+        this.topics = topics;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", fullName='" + fullName + '\'' +
+                ", email='" + email + '\'' +
+                ", statistics=" + statistics +
+                ", savedQuestions=" + savedQuestions +
+                ", roles=" + roles +
+                ", topics=" + topics +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id && Objects.equals(fullName, person.fullName) && Objects.equals(email, person.email) && Objects.equals(password, person.password) && Objects.equals(statistics, person.statistics) && Objects.equals(savedQuestions, person.savedQuestions) && Objects.equals(roles, person.roles) && Objects.equals(topics, person.topics);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fullName, email, password, statistics, savedQuestions, roles, topics);
+    }
 }
