899ad4440e9c85ee93b3a294746415a4955f1970
AlexTsoy <Alexandrotsoy@yandex.ru>
2023-01-02 11:56:00 +0300

    Add basic security.

--- src/main/java/com/project/green/entities/Role.java
+++ src/main/java/com/project/green/entities/Role.java
@@ -1,14 +1,6 @@
 package com.project.green.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.SequenceGenerator;
+import javax.persistence.*;
 import java.util.Objects;
 import java.util.Set;
 
